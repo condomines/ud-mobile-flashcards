d2b66b7438f9cf4f2a234dc6bb55e50dfe298c0d
Christophe Condomines <Chris.Condomines@gmail.com>
2020-05-23 12:19:17 +0200

    Feat: Add app container, tab navigator, first component structure.

--- App.js
+++ App.js
@@ -1,18 +1,62 @@
 import * as React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
+import { createAppContainer } from 'react-navigation';
+
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+import Decks from './components/Decks'
+import AddDeck from './components/AddDeck'
+
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
 
+
+const Tabs = createBottomTabNavigator({
+  Decks: {
+    screen: Decks,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name="cards" size={24} color={ tintColor }/>
+    },
+  },
+  AddDeck: {
+    screen: Decks,
+    navigationOptions: {
+      tabBarLabel: 'Add Deck',
+      tabBarIcon: ({ tintColor }) => <Entypo name="circle-with-plus" size={24} color={ tintColor } />
+    },
+  }
+} , {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    style: {
+      height: 56,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
+
+const AppContainer = createAppContainer(Tabs);
+
 export default function App() {
   return (
+    <AppContainer>
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
+      <Tabs />
     </View>
+    </AppContainer>
   );
 }
 
