2b6b3d9857e6be376b1896af11638f87f2bd3af0
Christophe Condomines <Chris.Condomines@gmail.com>
2020-05-23 12:22:11 +0200

    Fix: add missing components directory

---
+++ components/Decks.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { ScrollView, View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native'
+
+class Decks extends Component {
+	render () {
+		return (
+			<View>
+				<Text>Hola</Text>
+								<Text>Hola</Text>
+
+			</View>
+		)
+	}
+}
+
+export default Decks
