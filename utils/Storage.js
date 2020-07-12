d393342c18581aef16f4219276bf2bb302462802
Christophe Condomines <Chris.Condomines@gmail.com>
2020-07-12 11:17:00 +0200

    feat: Start to use async storage.

--- utils/Storage.js
+++ utils/Storage.js
@@ -1,32 +1,46 @@
-export function getDecks () {
-    return new Promise ((res,rej) => {
-        res({
-        1: {
-			id: '1',
-          name: 'React',
-          cards: [
-            {
-              question: 'What is React?',
-              answer: 'A library for managing user interfaces'
-            },
-            {
-              question: 'Where do you make Ajax requests in React?',
-              answer: 'The componentDidMount lifecycle event'
-            }
-          ]
+import { AsyncStorage } from 'react-native'
+
+const STORAGE_KEY = 'UD-Mobile-Flashcards-Decks'
+
+const demoData = {
+  1: {
+      id: '1',
+      name: 'React',
+      cards: [
+        {
+          question: 'What is React?',
+          answer: 'A library for managing user interfaces'
         },
-        2: {
-			id: '2',
-            name: 'JavaScript',
-            cards: [
-            {
-              question: 'What is a closure?',
-              answer: 'The combination of a function and the lexical environment within which that function was declared.'
-            }
-          ]
+        {
+          question: 'Where do you make Ajax requests in React?',
+          answer: 'The componentDidMount lifecycle event'
         }
+      ]
+  },
+  2: {
+      id: '2',
+      name: 'JavaScript',
+      cards: [
+      {
+        question: 'What is a closure?',
+        answer: 'The combination of a function and the lexical environment within which that function was declared.'
+      }
+    ]
+  }
+}
+
+export function getDecks () {
+  return AsyncStorage.getItem(STORAGE_KEY)
+    .then(
+      (res) => {
+        if (res === null) {
+          console.log('Generating demo data')
+          AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(demoData))
+          return demoData
+        }
+        console.log('Using the local store')
+        return JSON.parse(res)
       })
-    })
 }
 
 
